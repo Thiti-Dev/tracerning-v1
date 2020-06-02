028baf4d9c0a65e43507075897920c2641c9fdd3
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-02 16:06:14 +0700

    :wrench: [blogs-route]: Changed route path (conflict-prevention)

--- src/routes/blogs.ts
+++ src/routes/blogs.ts
@@ -7,7 +7,7 @@ import {protect} from '../middleware/authorization'
 
 router.route('/').post(protect,createBlog).get(getAllBlogs)
 router.route('/:blogId').delete(protect,deleteBlog).patch(protect,editBlog).get(getSpecificBlogData)
-router.route('/:username').get(getBlogOfUser)
-router.route('/:username/:slug').get(getBlogDataOfUser)
+router.route('/i/:username').get(getBlogOfUser)
+router.route('/i/:username/:slug').get(getBlogDataOfUser)
 
 export default router;
