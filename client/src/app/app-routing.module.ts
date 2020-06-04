c88cc7527e278a064b1b55845e87abfa1fbbb77a
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-05 00:00:57 +0700

    :space_invader: [router]: Added registration to /register, restructured (html-main)app.component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegistrationComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
