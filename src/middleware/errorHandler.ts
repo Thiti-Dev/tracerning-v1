4adc5e5eb1ca209750cae7f395408f82d5847bbd
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-06 00:06:57 +0700

    :m::zap: [errorHandler]: Implemented ValidationError&Duplicated field of mongoose

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,19 +1,47 @@
 import { Request, Response, NextFunction } from "express"
-
-interface ErrorResponse extends Error{
-    statusCode?: number
+import ErrorResponse from "../utils/errorResponse";
+interface ErrorResponseExtended extends ErrorResponse{
+	code?: number;
+	message: string|any;
+	errors?: any;
+	errCode?: number;
 }
 
-const errorHandler = (err:ErrorResponse,req:Request,res:Response,next:NextFunction) => {
+const errorHandler = (err:ErrorResponseExtended,req:Request,res:Response,next:NextFunction) => {
 	let error = { ...err };
 
 	error.message = err.message;
+	error.errCode = 0
 
-    console.log(err)
+	console.log(err)
+
+	// Mongoose duplicate key
+	if (err.code === 11000) {
+		const message = 'Duplicate field value entered';
+		error = new ErrorResponse(message, 400);
+		error.errCode = 1
+	}
+	
+	// Mongoose validation error
+	if (err.name === 'ValidationError') {
+		// Long ass string => old ways
+		//const message = Object.values(err.errors).map((val) => val.message);
+		//error = new ErrorResponse(errors, 400);
+		const errors : {
+			[key:string]: string;
+		} = {};
+		Object.values<{
+			[key:string]: string;
+		}>(err.errors).forEach((val) => (errors[val.path] = val.message));
+		error.message = errors; // new assigned way
+		error.statusCode = 400;
+		error.errCode = 2
+	}
 
     res.status(error.statusCode || 500).json({
 		success: false,
-		errors: error.message || 'Server Error'
+		errors: error.message || 'Server Error',
+		code: error.errCode
 	});
 }
 
